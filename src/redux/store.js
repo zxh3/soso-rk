c85d32c3c84ae14a728012f01f04ecf4f6ac69ce
xz2 <xz2@illinois.edu>
2019-04-18 23:29:55 -0500

    fixed a tiny bug

--- src/redux/store.js
+++ src/redux/store.js
@@ -11,7 +11,7 @@ const store = createStore(rootReducer, composeEnhancers(
   applyMiddleware(thunk)
 ));
 
-auth().onAuthStateChanged((authUser) => {
+auth.onAuthStateChanged((authUser) => {
   store.dispatch(changeAuthState(authUser));
 });
 
